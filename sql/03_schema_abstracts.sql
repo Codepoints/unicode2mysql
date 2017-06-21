cdf43fd281d2339d6e95bf3ea8dd9e34aa4b672f
Manuel Strehl <git@manuel-strehl.de>
2017-06-22 01:53:32 +0200

    initial commit

---
+++ sql/03_schema_abstracts.sql
@@ -0,0 +1,38 @@
+-- vim: ft=mysql
+
+--
+-- wikipedia abstract of a codepoint
+--
+CREATE TABLE codepoint_abstract (
+  cp       INTEGER REFERENCES codepoints,
+  abstract MEDIUMTEXT,
+  lang     VARCHAR(6) DEFAULT 'en',
+  UNIQUE ( cp, lang )
+);
+CREATE INDEX codepoint_abstract_cp ON codepoint_abstract ( cp );
+CREATE INDEX codepoint_abstract_cp_lang ON codepoint_abstract ( cp, lang );
+
+--
+-- wikipedia abstract of a Unicode block
+--
+CREATE TABLE block_abstract (
+  block    VARCHAR(255) REFERENCES blocks,
+  abstract MEDIUMTEXT,
+  lang     VARCHAR(6) DEFAULT 'en',
+  UNIQUE ( block, lang )
+);
+CREATE INDEX block_abstract_block ON block_abstract ( block );
+CREATE INDEX block_abstract_block_lang ON block_abstract ( block, lang );
+
+--
+-- Wikipedia abstract of a script
+--
+CREATE TABLE script_abstract (
+  sc       VARCHAR(4) REFERENCES scripts,
+  abstract MEDIUMTEXT,
+  lang     VARCHAR(6) DEFAULT 'en',
+  src      VARCHAR(255) -- explicitly give a source, since the Wikipedia name
+                        -- might not be obvious.
+);
+CREATE INDEX script_abstract_sc ON script_abstract ( sc );
+CREATE INDEX script_abstract_sc_lang ON script_abstract ( sc, lang );
