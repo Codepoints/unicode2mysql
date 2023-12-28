c3c9930399a1cfc406cfd658badd9559fa8b7c26
Manuel Strehl <git@manuel-strehl.de>
2023-12-28 11:16:11 +0100

    fixate charset and collation for prop tables

--- sql/01_schema_props.sql
+++ sql/01_schema_props.sql
@@ -4,13 +4,13 @@
 -- property lookup tables
 --
 
-CREATE TABLE prop_age ( age VARCHAR(4) PRIMARY KEY );
+CREATE TABLE prop_age ( age VARCHAR(4) PRIMARY KEY ) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 INSERT INTO  prop_age VALUES ('1.1'), ('2.0'), ('2.1'), ('3.0'), ('3.1'),
 ('3.2'), ('4.0'), ('4.1'), ('5.0'), ('5.1'), ('5.2'), ('6.0'), ('6.1'),
 ('6.2'), ('6.3'), ('7.0'), ('8.0'), ('9.0'), ('10.0'), ('11.0'), ('12.0'),
 ('12.1'), ('13.0'), ('14.0'), ('15.0'), ('15.1');
 
-CREATE TABLE prop_gc ( gc VARCHAR(2) PRIMARY KEY );
+CREATE TABLE prop_gc ( gc VARCHAR(2) PRIMARY KEY ) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 INSERT INTO  prop_gc VALUES
 ('Cc'), ('Cf'), ('Co'), ('Cn'), ('Cs'),
 ('Ll'), ('Lm'), ('Lo'), ('Lt'), ('Lu'),
@@ -20,7 +20,7 @@ INSERT INTO  prop_gc VALUES
 ('Sc'), ('Sk'), ('Sm'), ('So'),
 ('Zl'), ('Zp'), ('Zs');
 
-CREATE TABLE prop_bc ( bc VARCHAR(3) PRIMARY KEY );
+CREATE TABLE prop_bc ( bc VARCHAR(3) PRIMARY KEY ) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 INSERT INTO  prop_bc VALUES
 ('AL'), ('AN'),
 ('B'), ('BN'),
@@ -35,27 +35,27 @@ INSERT INTO  prop_bc VALUES
 ('S'),
 ('WS');
 
-CREATE TABLE prop_bpt ( bpt VARCHAR(1) PRIMARY KEY );
+CREATE TABLE prop_bpt ( bpt VARCHAR(1) PRIMARY KEY ) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 INSERT INTO  prop_bpt VALUES ('c'), ('n'), ('o');
 
-CREATE TABLE prop_dt ( dt VARCHAR(4) PRIMARY KEY );
+CREATE TABLE prop_dt ( dt VARCHAR(4) PRIMARY KEY ) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 INSERT INTO  prop_dt VALUES ('can'), ('com'), ('enc'), ('fin'), ('font'),
 ('fra'), ('init'), ('iso'), ('med'), ('nar'), ('nb'), ('none'), ('sml'),
 ('sqr'), ('sub'), ('sup'), ('vert'), ('wide');
 
-CREATE TABLE prop_nfc_qc ( nfc_qc VARCHAR(1) PRIMARY KEY );
+CREATE TABLE prop_nfc_qc ( nfc_qc VARCHAR(1) PRIMARY KEY ) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 INSERT INTO  prop_nfc_qc VALUES ('M'), ('N'), ('Y');
 
-CREATE TABLE prop_nfd_qc ( nfd_qc VARCHAR(1) PRIMARY KEY );
+CREATE TABLE prop_nfd_qc ( nfd_qc VARCHAR(1) PRIMARY KEY ) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 INSERT INTO  prop_nfd_qc VALUES ('N'), ('Y');
 
-CREATE TABLE prop_nt ( nt VARCHAR(4) PRIMARY KEY );
+CREATE TABLE prop_nt ( nt VARCHAR(4) PRIMARY KEY ) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 INSERT INTO  prop_nt VALUES ('De'), ('Di'), ('None'), ('Nu');
 
-CREATE TABLE prop_jt ( jt VARCHAR(1) PRIMARY KEY );
+CREATE TABLE prop_jt ( jt VARCHAR(1) PRIMARY KEY ) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 INSERT INTO  prop_jt VALUES ('C'), ('D'), ('L'), ('R'), ('T'), ('U');
 
-CREATE TABLE prop_jg ( jg VARCHAR(31) PRIMARY KEY );
+CREATE TABLE prop_jg ( jg VARCHAR(31) PRIMARY KEY ) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 INSERT INTO  prop_jg VALUES
 ('African_Feh'), ('African_Noon'), ('African_Qaf'), ('Ain'), ('Alaph'),
 ('Alef'), ('Alef_Maqsurah'),
@@ -92,7 +92,7 @@ INSERT INTO  prop_jg VALUES
 ('Yeh'), ('Yeh_Barree'), ('Yeh_With_Tail'), ('Yudh'), ('Yudh_He'),
 ('Zain'), ('Zhain');
 
-CREATE TABLE prop_lb ( lb VARCHAR(3) PRIMARY KEY );
+CREATE TABLE prop_lb ( lb VARCHAR(3) PRIMARY KEY ) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 INSERT INTO  prop_lb VALUES
 ('AI'), ('AK'), ('AL'), ('AP'), ('AS'),
 ('B2'), ('BA'), ('BB'), ('BK'),
@@ -114,13 +114,13 @@ INSERT INTO  prop_lb VALUES
 ('XX'),
 ('ZW'), ('ZWJ');
 
-CREATE TABLE prop_ea ( ea VARCHAR(2) PRIMARY KEY );
+CREATE TABLE prop_ea ( ea VARCHAR(2) PRIMARY KEY ) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 INSERT INTO  prop_ea VALUES ('A'), ('F'), ('H'), ('N'), ('Na'), ('W');
 
-CREATE TABLE prop_hst ( hst VARCHAR(3) PRIMARY KEY );
+CREATE TABLE prop_hst ( hst VARCHAR(3) PRIMARY KEY ) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 INSERT INTO  prop_hst VALUES ('L'), ('LV'), ('LVT'), ('T'), ('V'), ('NA');
 
-CREATE TABLE prop_InSC ( InSC VARCHAR(27) PRIMARY KEY );
+CREATE TABLE prop_InSC ( InSC VARCHAR(27) PRIMARY KEY ) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 INSERT INTO  prop_InSC VALUES
 ('Avagraha'), ('Bindu'), ('Brahmi_Joining_Number'), ('Cantillation_Mark'),
 ('Consonant'), ('Consonant_Dead'), ('Consonant_Final'),
@@ -134,27 +134,27 @@ INSERT INTO  prop_InSC VALUES
 ('Syllable_Modifier'), ('Tone_Letter'), ('Tone_Mark'), ('Virama'), ('Visarga'),
 ('Vowel'), ('Vowel_Dependent'), ('Vowel_Independent');
 
-CREATE TABLE prop_InMC ( InMC VARCHAR(24) PRIMARY KEY );
+CREATE TABLE prop_InMC ( InMC VARCHAR(24) PRIMARY KEY ) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 INSERT INTO  prop_InMC VALUES ('Right'), ('Left'), ('Visual_Order_Left'),
 ('Left_And_Right'), ('Top'), ('Bottom'), ('Top_And_Bottom'), ('Top_And_Right'),
 ('Top_And_Left'), ('Top_And_Left_And_Right'), ('Bottom_And_Right'),
 ('Top_And_Bottom_And_Right'), ('Overstruck'), ('Invisible'), ('NA');
 
-CREATE TABLE prop_InPC ( InPC VARCHAR(24) PRIMARY KEY );
+CREATE TABLE prop_InPC ( InPC VARCHAR(24) PRIMARY KEY ) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 INSERT INTO  prop_InPC VALUES ('Bottom'), ('Bottom_And_Left'),
 ('Bottom_And_Right'), ('Left'), ('Left_And_Right'), ('NA'), ('Overstruck'),
 ('Right'), ('Top'), ('Top_And_Bottom'), ('Top_And_Bottom_And_Left'),
 ('Top_And_Bottom_And_Right'), ('Top_And_Left'), ('Top_And_Left_And_Right'),
 ('Top_And_Right'), ('Visual_Order_Left');
 
-CREATE TABLE prop_InCB ( InCB VARCHAR(24) PRIMARY KEY );
+CREATE TABLE prop_InCB ( InCB VARCHAR(24) PRIMARY KEY ) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 INSERT INTO  prop_InCB VALUES ('None'), ('Linker'),
 ('Consonant'), ('Extend');
 
-CREATE TABLE prop_vo ( vo VARCHAR(2) PRIMARY KEY );
+CREATE TABLE prop_vo ( vo VARCHAR(2) PRIMARY KEY ) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 INSERT INTO  prop_vo VALUES ('U'), ('R'), ('Tu'), ('Tr');
 
-CREATE TABLE prop_GCB ( GCB VARCHAR(3) PRIMARY KEY );
+CREATE TABLE prop_GCB ( GCB VARCHAR(3) PRIMARY KEY ) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 INSERT INTO  prop_GCB VALUES
 ('CN'), ('CR'),
 ('EB'), ('EBG'), ('EM'), ('EX'),
@@ -168,7 +168,7 @@ INSERT INTO  prop_GCB VALUES
 ('XX'),
 ('ZWJ');
 
-CREATE TABLE prop_WB ( WB VARCHAR(9) PRIMARY KEY );
+CREATE TABLE prop_WB ( WB VARCHAR(9) PRIMARY KEY ) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 INSERT INTO  prop_WB VALUES
 ('CR'),
 ('DQ'),
@@ -186,7 +186,7 @@ INSERT INTO  prop_WB VALUES
 ('WSegSpace'),
 ('ZWJ');
 
-CREATE TABLE prop_SB ( SB VARCHAR(2) PRIMARY KEY );
+CREATE TABLE prop_SB ( SB VARCHAR(2) PRIMARY KEY ) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 INSERT INTO  prop_SB VALUES
 ('AT'),
 ('CL'), ('CR'),
@@ -198,10 +198,10 @@ INSERT INTO  prop_SB VALUES
 ('UP'),
 ('XX');
 
-CREATE TABLE prop_relation ( relation VARCHAR(8) PRIMARY KEY );
+CREATE TABLE prop_relation ( relation VARCHAR(8) PRIMARY KEY ) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 INSERT INTO  prop_relation VALUES ('bmg'), ('dm'), ('FC_NFKC'), ('suc'),
 ('slc'), ('stc'), ('uc'), ('lc'), ('tc'), ('scf'), ('cf'), ('NFKC_CF'),
 ('bpb'), ('EqUIdeo'), ('NFKC_SCF');
 
-CREATE TABLE prop_annotation_type ( annotation_type VARCHAR(7) PRIMARY KEY );
+CREATE TABLE prop_annotation_type ( annotation_type VARCHAR(7) PRIMARY KEY ) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 INSERT INTO prop_annotation_type VALUES ('tag'), ('tts');
