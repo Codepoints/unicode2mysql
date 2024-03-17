9ebb296234f35b3f8fde32142f9f845d6df561c8
Manuel Strehl <git@manuel-strehl.de>
2024-03-17 22:17:15 +0100

    add version to search_index

--- sql/09_schema_search_index.sql
+++ sql/09_schema_search_index.sql
@@ -5,7 +5,8 @@
 --
 
 CREATE TABLE search_index (
-    cp    INTEGER NOT NULL REFERENCES codepoints(cp),
-    text  TEXT
+    cp      INTEGER NOT NULL REFERENCES codepoints(cp),
+    text    TEXT,
+    version VARCHAR(127) NOT NULL DEFAULT '00000000'
 ) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 CREATE FULLTEXT INDEX search_index_text ON search_index ( text );
