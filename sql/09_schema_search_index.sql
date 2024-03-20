6cd235280fa602d4d21ad482952f5ae3c1c683ac
Manuel Strehl <git@manuel-strehl.de>
2024-03-21 00:06:21 +0100

    make search_index.cp unique

--- sql/09_schema_search_index.sql
+++ sql/09_schema_search_index.sql
@@ -7,6 +7,7 @@
 CREATE TABLE search_index (
     cp      INTEGER NOT NULL REFERENCES codepoints(cp),
     text    TEXT,
-    version VARCHAR(127) NOT NULL DEFAULT '00000000'
+    version VARCHAR(127) NOT NULL DEFAULT '00000000',
+    CONSTRAINT search_index_unique_cp UNIQUE (cp)
 ) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 CREATE FULLTEXT INDEX search_index_text ON search_index ( text );
