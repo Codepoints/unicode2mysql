ab777f76b0404fa607a862a81cfc07073c385282
Manuel Strehl <git@manuel-strehl.de>
2024-04-08 00:51:23 +0200

    refine WP extract cleaning

--- bin/scrub_wp_html.py
+++ bin/scrub_wp_html.py
@@ -12,16 +12,23 @@ from lxml.html.clean import Cleaner
 
 src = sys.stdin.read()
 doc = fragment_fromstring('<div>%s</div>' % src)
-# allow_tags was derived from manually filtering
-# cat cache/abstracts/*/* | grep -ohiE '<[a-z0-9]+' | sort | uniq
-# kill_tags: The <annotation> tag is used by WP to add LaTeX metadata to
-# MathML. We do not need that.
-cleaner = Cleaner(allow_tags=(
-    'abbr', 'b', 'bdi', 'bdo', 'big', 'blockquote', 'br', 'center', 'cite',
-    'code', 'dd', 'dfn', 'dl', 'dt', 'em', 'h2', 'i', 'kbd', 'li', 'math',
-    'mfrac', 'mi', 'mn', 'mo', 'mover', 'mpadded', 'mroot', 'mrow', 'mspace',
-    'msqrt', 'mstyle', 'msub', 'msubsup', 'msup', 'mtable', 'mtd', 'mtext',
-    'mtr', 'munder', 'munderover', 'ol', 'p', 'pre', 'rp', 'rt', 'ruby', 's',
-    'small', 'span', 'strong', 'sub', 'sup', 'time', 'tt', 'u', 'ul', 'var',
-), safe_attrs_only=True, kill_tags=('annotation',))
+cleaner = Cleaner(
+    # allow_tags was derived from manually filtering
+    # cat cache/abstracts/*/* | grep -ohiE '<[a-z0-9]+' | sort | uniq
+    allow_tags=(
+        'abbr', 'b', 'bdi', 'bdo', 'big', 'blockquote', 'br', 'center', 'cite',
+        'code', 'dd', 'dfn', 'dl', 'dt', 'em', 'h2', 'i', 'kbd', 'li', 'math',
+        'mfrac', 'mi', 'mn', 'mo', 'mover', 'mpadded', 'mroot', 'mrow', 'mspace',
+        'msqrt', 'mstyle', 'msub', 'msubsup', 'msup', 'mtable', 'mtd', 'mtext',
+        'mtr', 'munder', 'munderover', 'ol', 'p', 'pre', 'rp', 'rt', 'ruby', 's',
+        'small', 'span', 'strong', 'sub', 'sup', 'time', 'tt', 'u', 'ul', 'var',
+    ),
+    safe_attrs_only=True,
+    # safe_attrs was derived, too, by looking at what is currently used:
+    # cat cache/abstracts/*/* | grep -ohiE ' [a-z0-9-]+' | sort | uniq
+    # width and height are used in MathML context
+    safe_attrs=('clear', 'dir', 'height', 'lang', 'title', 'width',),
+    # kill_tags: The <annotation> tag is used by WP to add LaTeX metadata to
+    # MathML. We do not need that.
+    kill_tags=('annotation',))
 print(tostring(cleaner.clean_html(doc), encoding='UTF-8').decode('UTF-8')[5:-6].strip() or '')
