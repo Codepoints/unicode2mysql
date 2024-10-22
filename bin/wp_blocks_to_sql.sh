04c7b8adac91dbd9db3a95928bd9b591183690f9
Manuel Strehl <git@manuel-strehl.de>
2024-10-22 22:32:03 +0200

    prevent fetching obsolete Unicode blocks from WP

--- bin/wp_blocks_to_sql.sh
+++ bin/wp_blocks_to_sql.sh
@@ -5,6 +5,7 @@
 #    SELECT DISTINCT ?lang ?name ?range ?article WHERE {
 #      ?block wdt:P31 wd:Q3512806 .
 #      ?block wdt:P5949 ?range .
+#      MINUS { ?block wdt:P1366 ?next . } # ignore superseded blocks
 #      ?article schema:about ?block ;
 #               schema:inLanguage ?lang ;
 #               schema:name ?name ;
@@ -12,7 +13,7 @@
 #      FILTER(?lang in ('en', 'de', 'pl', 'es')) .
 #    }
 #
-# Sandbox: https://w.wiki/xjk
+# Sandbox: https://w.wiki/Bdz7
 #
 # Code explanation: see wp_scripts_to_sql.sh, which does more or less the
 # same.
@@ -22,7 +23,7 @@ set -euo pipefail
 
 TARGET="$1"
 
-$CURL $CURL_OPTS 'https://query.wikidata.org/sparql?query=SELECT%20DISTINCT%20%3Flang%20%3Fname%20%3Frange%20%3Farticle%20WHERE%20%7B%0A%20%20%3Fblock%20wdt%3AP31%20wd%3AQ3512806%20.%0A%20%20%3Fblock%20wdt%3AP5949%20%3Frange%20.%0A%20%20%3Farticle%20schema%3Aabout%20%3Fblock%20%3B%0A%20%20%20%20%20%20%20%20%20%20%20%20%20%20schema%3AinLanguage%20%3Flang%20%3B%0A%20%20%20%20%20%20%20%20%20%20%20%20%20%20schema%3Aname%20%3Fname%20%3B%0A%20%20%20%20%20%20%20%20%20%20%20%20%20%20schema%3AisPartOf%20%5B%20wikibase%3AwikiGroup%20%22wikipedia%22%20%5D%20.%0A%20%20FILTER(%3Flang%20in%20(%22en%22%2C%20%22de%22%2C%20%22pl%22%2C%20%22es%22))%20.%0A%7D&format=json' | \
+$CURL $CURL_OPTS 'https://query.wikidata.org/sparql?query=SELECT%20DISTINCT%20%3Flang%20%3Fname%20%3Frange%20%3Farticle%20WHERE%20%7B%0A%3Fblock%20wdt%3AP31%20wd%3AQ3512806%20.%0A%3Fblock%20wdt%3AP5949%20%3Frange%20.%0AMINUS%20%7B%20%3Fblock%20wdt%3AP1366%20%3Fnext%20.%20%7D%0A%3Farticle%20schema%3Aabout%20%3Fblock%20%3B%0Aschema%3AinLanguage%20%3Flang%20%3B%0Aschema%3Aname%20%3Fname%20%3B%0Aschema%3AisPartOf%20%5B%20wikibase%3AwikiGroup%20%22wikipedia%22%20%5D%20.%0AFILTER%28%3Flang%20in%20%28%27en%27%2C%20%27de%27%2C%20%27pl%27%2C%20%27es%27%29%29%20.%0A%7D&format=json' | \
     $JQ -r '.results.bindings[] | [.range.value, .article.value, .lang.value, .name.value] | join("\t")' | \
     while IFS= read -r line; do
         IFS=$'\t'
